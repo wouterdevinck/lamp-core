f0744ea8d5f36c4642ca2db5ffc567bf195bfbf8
Wouter Devinck <wouter.devinck@barco.com>
2017-11-30 23:02:11 +0100

    Added ledboardchain interface and wrapper

--- LampImpl.h
+++ LampImpl.h
@@ -9,20 +9,20 @@
 
 namespace lamp {
 
-  //template<uint8_t N>
+  template<uint8_t N>
   class Lamp {
 
     public:
       explicit Lamp(IHttpServer* http, IRgbLed* led, ILedBoardChain* leds) : 
         _http(http), _handler(new HttpHandler(led)), _led(led), _leds(leds) {}
 
-      void start(int port) const;
+      void start(int port) const; // move port to template?
 
     private:
       IHttpServer* _http;
       IHttpHandler* _handler;
       IRgbLed* _led;
-	  ILedBoardChain* _leds;
+	  ILedBoardChain<N>* _leds;
 
   };
 
